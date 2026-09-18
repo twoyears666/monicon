032632415d4890e902211ce28078c16b61527e98
twoyears666 <1638290186@qq.com>
2026-09-18 20:38:20 +1000

    Install crash diagnostics before app startup

--- Monicon/MoniconApp.swift
+++ Monicon/MoniconApp.swift
@@ -2,7 +2,12 @@ import SwiftUI
 
 @main
 struct MoniconApp: App {
-    @StateObject private var capture = CaptureSessionManager()
+    @StateObject private var capture: CaptureSessionManager
+
+    init() {
+        MoniconDiagnostics.install()
+        _capture = StateObject(wrappedValue: CaptureSessionManager())
+    }
 
     var body: some Scene {
         WindowGroup {
